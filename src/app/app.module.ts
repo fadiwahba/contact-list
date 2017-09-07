b29f29cbcb781b766f4521bdcd8e2894bcd7d12c
Fady Wahba <fady.wahba@mondiamedia.com>
2017-09-07 20:32:20 +0200

    setup basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
